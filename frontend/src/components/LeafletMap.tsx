d662ea906ab66221b2459c2506d9c1e6d73f95e5
Ravinder Jilkapally <jravinderreddy@gmail.com>
2026-02-06 20:45:45 -0600

    Switch to dark map tiles

--- frontend/src/components/LeafletMap.tsx
+++ frontend/src/components/LeafletMap.tsx
@@ -426,7 +426,7 @@ export function LeafletMap({
         {/* Light tiles from CartoDB for better visibility */}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
           maxZoom={20}
         />
 
